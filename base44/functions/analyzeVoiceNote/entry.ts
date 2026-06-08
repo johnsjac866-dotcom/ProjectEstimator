559f12efd51d3ff26179fc4034aa104243b50fc8
Jacob Johnson <it@aspenmadison.space>
2026-06-08 18:11:19 +0000

    File changes

--- base44/functions/analyzeVoiceNote/entry.ts
+++ base44/functions/analyzeVoiceNote/entry.ts
@@ -13,7 +13,7 @@ Deno.serve(async (req) => {
     const transcripts = [];
     for (const url of dataUrls) {
       try {
-        const res = await base44.asServiceRole.integrations.Core.TranscribeAudio({ file_url: url });
+        const res = await base44.asServiceRole.integrations.Core.TranscribeAudio({ audio_url: url });
         transcripts.push(res.transcript || '');
       } catch (err) {
         console.error('Transcribe error for', url, ':', err.message);
